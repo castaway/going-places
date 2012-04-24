5926d3eb149f996ce5389e4bb98d739d055748c6
Jess Robinson <castaway@desert-island.me.uk>
2012-04-25 00:17:39 +0100

    Switch to use template wrappers start of page for place/card

--- lib/GoingPlaces/View.pm
+++ lib/GoingPlaces/View.pm
@@ -16,10 +16,32 @@ has 'tt' => (is => 'ro', lazy => 1, builder => '_build_tt');
 sub _build_tt {
     my ($self) = @_;
 
-    return Template->new({ INCLUDE_PATH => dir($self->app_cwd)->subdir('templates')->stringify});
+    return Template->new({ 
+        INCLUDE_PATH => dir($self->app_cwd)->subdir('templates')->stringify,
+        WRAPPER      => 'wrapper.tt',
+                         });
 }
 
 sub get_default_page {
+}
+
+sub place_page {
+    my ($self, $card_row, $user_row) = @_;
+
+    # pre-template data mungings
+    my $has_card = $user_row && $user_row->user_cards_rs->find({ $card_row->id });
+    
+    my $output = '';
+    $self->tt->process('place_page.tt',
+                       {
+                           user => $user_row,
+                           card => $card_row,
+                           has_card => $has_card,
+                       },
+                       \$output )
+        || die $self->tt->error;
+
+    return $output;
 }
 
 sub map_page {
